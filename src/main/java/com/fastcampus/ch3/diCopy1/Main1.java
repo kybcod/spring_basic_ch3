ee1ad2bb411bb03e2493523b5a1b4f83af7e043b
kyb <kyb-0215@naver.com>
2024-12-17 20:18:10 +0900

    Spring DI : Object로 변환

--- src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
+++ src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
@@ -6,18 +6,20 @@ import java.util.Properties;
 class Car{}
 class SprotCar extends Car {}
 class Truck extends Car {}
+class  Engine{}
 
 public class Main1 {
-    public static void main(String[] args) {
-        Car car = new SprotCar();
+    public static void main(String[] args) throws Exception {
+        Car car = (Car) getObject("car");
+        Engine engine = (Engine) getObject("engine");
         System.out.println("car = " + car);
     }
 
-    static Car getCar() throws Exception {
+    static Object getObject(String key) throws Exception {
         Properties prop = new Properties();
         prop.load(new FileReader("cofig.txt"));
 
-        Class clazz = Class.forName(prop.getProperty("car"));
-        return (Car) clazz.newInstance();
+        Class clazz = Class.forName(prop.getProperty(key));
+        return clazz.newInstance();
     }
 }
