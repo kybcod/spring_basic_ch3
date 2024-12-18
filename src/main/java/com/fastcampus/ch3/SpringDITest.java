c12a3ef176a6fc5693c00706c8cb5bf305444207
kyb <kyb-0215@naver.com>
2024-12-18 20:17:14 +0900

    Spring DI : 값 주입

--- src/main/java/com/fastcampus/ch3/SpringDITest.java
+++ src/main/java/com/fastcampus/ch3/SpringDITest.java
@@ -1,15 +1,38 @@
 package com.fastcampus.ch3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Arrays;
 
+@Component("engine") class Engine{} // <bean id="engine" class"com.fastcampus.ch3.Engine"/>
+@Component class SuperEngine extends Engine {}
+@Component class TurboEngine extends Engine {}
+@Component  class Door{}
+@Component
 class Car{
-    String color;
-    int oil;
-    Engine engine;
+    @Value("red") String color;
+    @Value("100") int oil;
+    //@Autowired @Qualifier("superEngine") => byType
+    // == @Resource(name = "superEngine") => byName
+    @Resource(name = "superEngine") //
+    Engine engine; // byType - 타입으로 먼저 검색, 여러개면 이름으로 검색 - engine, superEngine, turboEngine
+    @Autowired
     Door[] doors;
+
+    public Car() {} //기본 생성자 꼭 만들기
+
+    public Car(String color, int oil, Engine engine, Door[] doors) {
+        this.color = color;
+        this.oil = oil;
+        this.engine = engine;
+        this.doors = doors;
+    }
 
     public void setColor(String color) {
         this.color = color;
@@ -27,7 +50,6 @@ class Car{
         this.doors = doors;
     }
 
-
     @Override
     public String toString() {
         return "Car{" +
@@ -38,8 +60,6 @@ class Car{
                 '}';
     }
 }
-class Engine{}
-class Door{}
 
 public class SpringDITest {
     public static void main(String[] args) {
@@ -48,15 +68,17 @@ public class SpringDITest {
         Car car2 = context.getBean("car2", Car.class); //byType
         // car, car2 같은 문장
 
-        Engine engine = (Engine)context.getBean("engine");
+        // Engine engine = (Engine)context.getBean("engine"); //byName
+        // Engine engine = (Engine)context.getBean(Engine.class); //byType - 같은 타입이 여러 개일 경우 오류
+        Engine engine = (Engine)context.getBean("superEngine"); //byType
         Door door = (Door)context.getBean("door");
 
-        // xml 파일에 값이 존재하기 때문에 생략 가능
-        /*car.setColor("red");
+        // set or autowired or resource 해주어야 함
+        car.setColor("red");
         car.setOil(100);
         car.setEngine(engine);
         car.setDoors(new Door[]{context.getBean("door", Door.class), (Door) context.getBean("door")});
-*/
+
         System.out.println("car = " + car);
         System.out.println("engine = " + engine);
         System.out.println("door = " + door);
